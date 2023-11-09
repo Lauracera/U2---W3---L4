7de8394f6bbdac007b8e0166f424913eca749a70
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-11-09 16:49:42 +0100

    prova

--- script/script.js
+++ script/script.js
@@ -2,22 +2,30 @@ const URL = "https://api.pexels.com/v1/search?query=flower";
 const apiKey = "OTwFoLY3KBHQlqt3HHFkS6GrGDd5QRnOmlzZoTmMiacbdNJ6g22YyXC8";
 const loadImagesBtn = document.getElementById("loadImages");
 const loadSecondaryImagesBtn = document.getElementById("loadSecondaryImages");
+const card = document.getElementsByClassName("card");
 
 window.onload = () => {
   fetch(URL, {
     headers: {
-      Authorization:
-        "[OTwFoLY3KBHQlqt3HHFkS6GrGDd5QRnOmlzZoTmMiacbdNJ6g22YyXC8]",
+      Authorization: apiKey,
     },
   })
     .then((resp) => resp.json())
     .then((resourceObj) => {
-      const { url, photographer, alt } = resourceObj;
-
-      document.getElementById("url").value = url;
-      document.getElementById("photographer").value = photographer;
-      document.getElementById("alt").value = alt;
+      console.log(resourceObj);
+      resourceObj.forEach((elem) => {
+        const imgCard = document.createElement("img");
+        imgCard.src = elem.URL;
+        imgCard.className = "card-img-top object-fit-cover";
+        card.appendChild(imgCard);
+      });
     })
 
     .catch((err) => console.log(err));
 };
+
+// const { url, photographer, alt } = resourceObj;
+
+//       document.getElementById("url").value = url;
+//       document.getElementById("photographer").value = photographer;
+//       document.getElementById("alt").value = alt;
