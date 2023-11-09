5d577548dc8a45c555bec21eb92c5109d9fd3174
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-11-09 15:58:19 +0100

    secondo .then + .catch

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 const URL = "https://api.pexels.com/v1/search?query=flower";
 const apiKey = "OTwFoLY3KBHQlqt3HHFkS6GrGDd5QRnOmlzZoTmMiacbdNJ6g22YyXC8";
+const loadImagesBtn = document.getElementById("loadImages");
+const loadSecondaryImagesBtn = document.getElementById("loadSecondaryImages");
 
 window.onload = () => {
   fetch(URL, {
@@ -11,5 +13,11 @@ window.onload = () => {
     .then((resp) => resp.json())
     .then((resourceObj) => {
       const { url, photographer, alt } = resourceObj;
-    });
+
+      document.getElementById("url").value = url;
+      document.getElementById("photographer").value = photographer;
+      document.getElementById("alt").value = alt;
+    })
+
+    .catch((err) => console.log(err));
 };
